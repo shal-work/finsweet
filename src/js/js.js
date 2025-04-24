4ad0ea3def782a62094fc21f559f9c18258aa698
shal-work <shal.work@mail.ru>
2025-04-24 16:40:04 +0300

    third commit, fixed bug

--- src/js/js.js
+++ src/js/js.js
@@ -3,4 +3,4 @@ import './lib/site/main.js';
 
 
 console.log("%c  РОССИЯ  ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
-console.log("web-site DiveSea  for verstaem.online");
+console.log("web-site Finsweet for https://verstaem.online/");
